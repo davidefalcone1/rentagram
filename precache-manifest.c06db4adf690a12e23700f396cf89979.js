631b12155c9dc6928b3499c96ce2fb65938b697b
davidefalcone1 <davidefalcone1@gmail.com>
2021-06-19 00:15:05 +0200

    Updates

---
+++ precache-manifest.c06db4adf690a12e23700f396cf89979.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d57a4a14403037a61dbfc118435e721f",
+    "url": "/rentagram/index.html"
+  },
+  {
+    "revision": "0227c6129a9308ea4e71",
+    "url": "/rentagram/static/css/main.65f60944.chunk.css"
+  },
+  {
+    "revision": "b75ba8bc688187ae92f7",
+    "url": "/rentagram/static/js/2.ce828d0a.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/rentagram/static/js/2.ce828d0a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0227c6129a9308ea4e71",
+    "url": "/rentagram/static/js/main.7d19034e.chunk.js"
+  },
+  {
+    "revision": "1fe6d3a9e5cb224f64f6",
+    "url": "/rentagram/static/js/runtime-main.a50b21ca.js"
+  },
+  {
+    "revision": "b2da6bc2f4f713c9514001c067f58a9b",
+    "url": "/rentagram/static/media/icon_time_end.b2da6bc2.svg"
+  },
+  {
+    "revision": "f455fbbe866b2f5586946e4001e7e586",
+    "url": "/rentagram/static/media/icon_time_start.f455fbbe.svg"
+  }
+]);
